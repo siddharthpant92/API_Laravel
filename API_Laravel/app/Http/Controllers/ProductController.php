07c981799a48c27b06aae66a2d62310252df13ad
Siddharth Pant <siddharthpant92@gmail.com>
2018-08-20 16:01:16 -0600

    Created basic api

--- API_Laravel/app/Http/Controllers/ProductController.php
+++ API_Laravel/app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\Product as ProductResource;
+use App\Product;
 
-class ProductController extends Controller
+class ProductController extends Controller 
 {
-    //
+	public function show($id) 
+	{
+		return new ProductResource(Product::find($id));
+	}
 }
