1191d85cf67753bc4940f72b5e2341763ec6e52f
Siddharth Pant <siddharthpant92@gmail.com>
2018-08-21 17:46:10 -0600

    Handling null value in related product resource

--- API_Laravel/app/Http/Resources/RelatedProduct.php
+++ API_Laravel/app/Http/Resources/RelatedProduct.php
@@ -18,10 +18,21 @@ class RelatedProduct extends JsonResource
         $productId = $request->id;
         $relatedProduct = (new RelatedProductController)->getRelatedProduct($productId);
 
-        return [
-            'related_product' => $relatedProduct->related_product,
-            'individual_price' => $relatedProduct->individual_price,
-            'bundle_price' => $relatedProduct->bundle_price,
-        ];
+        if(isset($relatedProduct->related_product))
+        {
+            return [
+                'related_product' => $relatedProduct->related_product,
+                'individual_price' => $relatedProduct->individual_price,
+                'bundle_price' => $relatedProduct->bundle_price,
+            ];
+        }
+        else
+        {
+            return [
+                'related_product' => null,
+                'individual_price' => null,
+                'bundle_price' => null,
+            ];
+        }
     }
 }
